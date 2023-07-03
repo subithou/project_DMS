d315994d4bf7c12db9c4a7dd32be73a60437b4b3
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-03 17:21:13 +0530

    display every column of bd in admin panel

--- hod/admin.py
+++ hod/admin.py
@@ -5,7 +5,7 @@ from hod.models import batch, scheme, subject, subject_to_staff
 # Register your models here.
 @admin.register(batch)
 class Batches(admin.ModelAdmin):
-    pass
+    list_display = ('class_name', 'date_of_join','semester','scheme','tutor_id')
 
 @admin.register(subject)
 class Subject(admin.ModelAdmin):
@@ -13,7 +13,7 @@ class Subject(admin.ModelAdmin):
 
 @admin.register(scheme)
 class Scheme(admin.ModelAdmin):
-    pass
+    list_display = ('scheme')
 
 @admin.register(subject_to_staff)
 class Scheme(admin.ModelAdmin):
