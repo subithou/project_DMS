da98f1466a6bb05daf76aea11a687d0b80a00b16
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-03 11:19:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --noinput first_name --noinput last_name --noinput
+python manage.py createsuperuser --username --noinput --first_name admin --last_name lbscek
