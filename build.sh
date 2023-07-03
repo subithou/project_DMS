111dc6c0b7ae9b46da265de3828838ed486d5ef6
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-03 15:07:55 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+python manage.py createsuperuser --username admin1 --first_name admin1 --last_name user1 --noinput
+
+
+# echo "from django.contrib.auth import get_user_model;
+# User = get_user_model();
+# user = User.objects.get(username='admin');
+# user.first_name='Admin';
+# user.last_name='User';
+# user.save()" | python manage.py shell 
