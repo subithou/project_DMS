40f2084d7a0561492eba2198380c84a7417fc371
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-03 11:53:22 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username admin --first_name Admin --last_name User --noinput
+python manage.py createsuperuser --username admin1 --first_name admin1 --last_name user1 --noinput
 
 
-echo "from django.contrib.auth import get_user_model;
-User = get_user_model();
-user = User.objects.get(username='admin');
-user.first_name='Admin';
-user.last_name='User';
-user.save()" | python manage.py shell 
+# echo "from django.contrib.auth import get_user_model;
+# User = get_user_model();
+# user = User.objects.get(username='admin');
+# user.first_name='Admin';
+# user.last_name='User';
+# user.save()" | python manage.py shell 
