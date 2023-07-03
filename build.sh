40bb3af21b7f2c10083e25e0680d41130472a91b
Subith O U <57960146+subithou@users.noreply.github.com>
2023-07-03 11:07:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --no-input
+DJANGO_SUPERUSER_PASSWORD=12345 DJANGO_SUPERUSER_USERNAME=pacho DJANGO_SUPERUSER_EMAIL=pacho@gmail.com python manage.py createsuperuser --noinput
